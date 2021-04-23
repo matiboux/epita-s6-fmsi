4689c8c119ce221dd958f80b3e1499b7e5c7196a
Benjamin Peter <benjamin.peter@epita.fr>
2021-04-23 16:27:18 +0200

    genPrime: Optimized sieve eratosthene with less loops and less comparisons
    1st opti: Reduced the number of loops by stoping when i*i <= n
    2nd opti: Set all the pair number > 2 to False to avoid checking pair
    numbers

--- genPrime.py
+++ genPrime.py
@@ -14,33 +14,37 @@ def is_prime(n):
   if n%3 == 0: return False
   r = int(n**0.5) 
   f = 5
-  
+
   while f <= r:
     if n % f == 0: return False
     if n % (f+2) == 0: return False
     f += 6
-  return True    
+  return True
 
 def erathosthene(n):
-    
+
     res = [True] * n
     res_p = []
     res[0] = False
     res[1] = False
-    
+    for i in range(4, n, 2):
+        res[i] = False
+
     if n < 2:
         return []
 
-    for i in range(2, n):      
+    i = 3
+    while i * i <= n:
         if res[i]:
             res_p.append(i)
-            for j in range(i + i, n, i):
+            for j in range(i * i, n, 2 * i):
                 res[j] = False
-                
+        i += 1
+
     return res_p
 
-table =erathosthene(1000)
+table =erathosthene(100000)
 count = 0
 
 for n in table:
-    print(n)
+    assert(is_prime(n))
