4a45cb67cd9b5c7e8240be88df1c190c996ea91b
Bastien <bastien.coutadeur@epita.fr>
2021-04-23 16:11:31 +0200

    added erathosthene

---
+++ genPrime.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 23 15:38:13 2021
+
+@author: Bastien
+"""
+
+import math as m
+
+def is_prime(n):
+  if n == 2 or n == 3: return True
+  if n < 2 or n%2 == 0: return False
+  if n < 9: return True
+  if n%3 == 0: return False
+  r = int(n**0.5) 
+  f = 5
+  while f <= r:
+    if n % f == 0: return False
+    if n % (f+2) == 0: return False
+    f += 6
+  return True    
+
+def erathosthene(n):
+    
+    res = [True] * n
+    res[0] = False
+    res[1] = False
+    
+    if n < 2:
+        return []
+
+    for i in range(2, n):       
+        
+        if res[i]:
+            for j in range(i + i, n, i):
+                res[j] = False
+                
+    return res
+
+
+table =erathosthene(1200) 
+
+for i in range(len(table)):
+    if table[i]:
+        assert(is_prime(i))
