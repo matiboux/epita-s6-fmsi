4330750e3193fb9a8153063fbf0d7ecce64945bc
Matiboux <matiboux@gmail.com>
2021-04-24 15:40:21 +0200

    genPrime.py: Disable test code execution for when importing

--- genPrime.py
+++ genPrime.py
@@ -5,21 +5,21 @@ Created on Fri Apr 23 15:38:13 2021
 @author: Bastien
 """
 
-import math as m
-
 def is_prime(n):
-  if n == 2 or n == 3: return True
-  if n < 2 or n%2 == 0: return False
-  if n < 9: return True
-  if n%3 == 0: return False
-  r = int(n**0.5) 
-  f = 5
 
-  while f <= r:
-    if n % f == 0: return False
-    if n % (f+2) == 0: return False
-    f += 6
-  return True
+    if n == 2 or n == 3: return True
+    if n < 2 or n % 2 == 0: return False
+    if n < 9: return True
+    if n % 3 == 0: return False
+    r = int(n ** 0.5) 
+    f = 5
+
+    while f <= r:
+        if n % f == 0: return False
+        if n % (f + 2) == 0: return False
+        f += 6
+
+    return True
 
 def erathosthene(n):
 
@@ -49,8 +49,9 @@ def erathosthene(n):
 
     return res_p
 
-table =erathosthene(1000)
-count = 0
+if __name__ == "__main__":
+    table = erathosthene(1000)
+    count = 0
 
-for n in table:
-    assert(is_prime(n))
+    for n in table:
+        assert(is_prime(n))
