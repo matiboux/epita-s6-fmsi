b0fa46790debb85529397b7ba4fa15de398ff268
Matiboux <matiboux@gmail.com>
2021-04-24 17:15:31 +0200

    cracking.py: Fixed code for testing Pollard's p-1 algorithm

--- cracking.py
+++ cracking.py
@@ -4,15 +4,21 @@ Editor de Spyder
 
 Este es un archivo temporal.
 """
+
 import math
 import rsa
 
+"""
+Pollard's p-1 algorithm
 
-def n_minus_one(n, B):
+@param n the RSA ciphering modulus
+@param b the limit for iterating
+"""
+def p_minus_one(n, b):
 
     a = 2
     
-    for j in range(2, B + 1):
+    for j in range(2, b + 1):
         a = pow(a, j, n)
         d = math.gcd(a - 1, n)
         
@@ -23,13 +29,13 @@ def n_minus_one(n, B):
     
 def crack_primes(n):
     i = 2
-    r = n_minus_one(n, i)
+    r = p_minus_one(n, i)
     
     while r == -1:
-        r = n_minus_one(n, i)
+        r = p_minus_one(n, i)
         i += 1
         
-        if (i > 2**16):
+        if (i > 2 ** 16):
             return None
     p = r
     q = n // r
@@ -42,31 +48,20 @@ def crack_msg(msg, n):
         print("Could not crack message")
         return None
     
-    p,q = primes
-    r = rsa.RSA.generate(p,q)
+    (p, q) = primes
+    r = rsa.RSA.generate(p, q)
     
-    return r.decrypt(msg)    
- 
-
-def message_to_int(a_string):
-    
-    a_byte_array = bytearray(a_string, "utf8")
-    
-    return int.from_bytes(a_byte_array, byteorder='big')
-
-def int_to_message(msg):
-    
-    byte  = msg.to_bytes((msg.bit_length() + 7) // 8, byteorder='big')
-    return byte.decode("utf-8")
+    return r.decrypt(msg)
 
 if __name__ == "__main__":
-    original = "hello world!!"
-    message = message_to_int(original)
+    original_msg = "Hello, world! This is my very secret message."
+    print("Encrypting:", original_msg)
 
     r = rsa.RSA.generate(661, 673)
-    m = r.encrypt(message)
-    
-    print(10 *"*" + "Cracking" + 10 * "*")
-    msg = crack_msg(m, r.n)
-    print("got:", int_to_message(msg))
-    print("original:", original)
+    encrypted_msg = r.encrypt(original_msg)
+    # print("Encrypted data:", encrypted_msg)
+
+    print(10 * '*', "Cracking", 10 * '*')
+    cracked_msg = crack_msg(encrypted_msg, r.n)
+    print("Got:", cracked_msg)
+    print("Success!" if cracked_msg == original_msg else "Failure.")
