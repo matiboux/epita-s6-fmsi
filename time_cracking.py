3e6bcb9f3976128687b1a0178fbb3c0739b6b683
Matiboux <matiboux@gmail.com>
2021-04-25 19:16:00 +0200

    Refactor time_cracking.py

--- time_cracking.py
+++ time_cracking.py
@@ -6,12 +6,14 @@ import fermat as ferm
 import miller_rabin as mil
 from rsa import RSA
 import time
+
 def time_crack(func, max_prime):
     era = prim.erathosthene(max_prime)
     sucess = 0
     failed = 0
     time_array = []
     prime_array = []
+
     for i in range(1,len(era),6):
         start_time = time.perf_counter()
         r = RSA.generate(era[i-1],era[i])
@@ -29,24 +31,21 @@ def time_crack(func, max_prime):
             p,q = cracked
             print("p = " + str(p) + " q = " + str(q) + " --- %s seconds ---" % time_used)
             sucess += 1
+
     print("Sucess rate: " + str(sucess) + "/" + str(failed))
     print(time_array)
     print(prime_array)
     return time_array, prime_array
 
-
 if __name__ == "__main__":
     time_crack(rho.crack_primes,10000)
-    #time_crack(ferm.crack_primes,1000)
-#    p = mil.generate_prime(32)
-#    q = mil.generate_prime(32)
-#    print("p = " + str(p) + " q = " + str(q) )
-#    n = p*q
-#    start_time = time.perf_counter()
-#    cracked = ferm.crack_primes(n)
-#    time_used =   round(time.perf_counter() - start_time,4)
-#    p,q = cracked
-#    print("p = " + str(p) + " q = " + str(q) + " --- %s seconds ---" % time_used)
-    
-
-
+    # time_crack(ferm.crack_primes,1000)
+    # p = mil.generate_prime(32)
+    # q = mil.generate_prime(32)
+    # print("p = " + str(p) + " q = " + str(q) )
+    # n = p*q
+    # start_time = time.perf_counter()
+    # cracked = ferm.crack_primes(n)
+    # time_used =   round(time.perf_counter() - start_time,4)
+    # p,q = cracked
+    # print("p = " + str(p) + " q = " + str(q) + " --- %s seconds ---" % time_used)
